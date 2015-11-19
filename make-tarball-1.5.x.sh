be144eb0ea01bd30ba8ec623efd2cb177e750dc2
Dieter Reuter <dieter.reuter@me.com>
2015-11-19 22:57:30 +0100

    Run tests for Go 1.5.x

--- make-tarball-1.5.x.sh
+++ make-tarball-1.5.x.sh
@@ -17,7 +17,7 @@ curl -sSL https://storage.googleapis.com/golang/go${GO_VERSION}.src.tar.gz | tar
 
 # now compile Go 1.5.x and package it as a tarball
 pushd /usr/local/go/src
-time ./make.bash --no-clean 2>&1
+time ./all.bash 2>&1
 cd ../..
 tar cfz go${GO_VERSION}.linux-armv${GOARM}.tar.gz ./go
 popd
