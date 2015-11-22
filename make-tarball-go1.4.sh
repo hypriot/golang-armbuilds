74d0b9e11bda99d090940be399492b22a68881e8
Dieter Reuter <dieter.reuter@me.com>
2015-11-22 13:40:39 +0100

    Improved bash script to prevent globbing and word splitting

--- make-tarball-go1.4.sh
+++ make-tarball-go1.4.sh
@@ -6,16 +6,16 @@ GOARM=${GOARM:-7}
 # fetch Go 1.4.x source tarball
 GO_VERSION=${GO_VERSION:-1.4.3}
 rm -fr /usr/local/go
-curl -sSL https://storage.googleapis.com/golang/go${GO_VERSION}.src.tar.gz | tar xz -C /usr/local
+curl -sSL "https://storage.googleapis.com/golang/go${GO_VERSION}.src.tar.gz" | tar xz -C /usr/local
 
 # now compile Go 1.4.x and package it as a tarball
 pushd /usr/local/go/src
 #time ./all.bash 2>&1
 time ./make.bash 2>&1
 cd ../..
-tar cfz go${GO_VERSION}.linux-armv${GOARM}.tar.gz go
+tar cfz "go${GO_VERSION}.linux-armv${GOARM}.tar.gz" go
 popd
-mv /usr/local/go${GO_VERSION}.linux-armv${GOARM}.tar.gz .
+mv "/usr/local/go${GO_VERSION}.linux-armv${GOARM}.tar.gz" .
 
 # cleanup
 rm -fr /usr/local/go
--- make-tarball-go1.5.sh
+++ make-tarball-go1.5.sh
@@ -9,24 +9,24 @@ GOROOT_BOOTSTRAP=$HOME/go1.4
 # install a pre-compiled Go 1.4.x tarball to bootstrap
 GO_BOOTSTRAP_GOARM=${GO_BOOTSTRAP_GOARM:-7}
 GO_BOOTSTRAP_VERSION=${GO_BOOTSTRAP_VERSION:-1.4.3}
-rm -fr $GOROOT_BOOTSTRAP
-mkdir -p $GOROOT_BOOTSTRAP
-curl -sSL https://github.com/hypriot/golang-armbuilds/releases/download/v${GO_BOOTSTRAP_VERSION}/go${GO_BOOTSTRAP_VERSION}.linux-armv${GO_BOOTSTRAP_GOARM}.tar.gz | tar xz -C $GOROOT_BOOTSTRAP --strip-components=1
+rm -fr "$GOROOT_BOOTSTRAP"
+mkdir -p "$GOROOT_BOOTSTRAP"
+curl -sSL "https://github.com/hypriot/golang-armbuilds/releases/download/v${GO_BOOTSTRAP_VERSION}/go${GO_BOOTSTRAP_VERSION}.linux-armv${GO_BOOTSTRAP_GOARM}.tar.gz" | tar xz -C "$GOROOT_BOOTSTRAP" --strip-components=1
 
 # fetch Go 1.5.x source tarball
 GO_VERSION=${GO_VERSION:-1.5.1}
 rm -fr /usr/local/go
-curl -sSL https://storage.googleapis.com/golang/go${GO_VERSION}.src.tar.gz | tar xz -C /usr/local
+curl -sSL "https://storage.googleapis.com/golang/go${GO_VERSION}.src.tar.gz" | tar xz -C /usr/local
 
 # now compile Go 1.5.x and package it as a tarball
 pushd /usr/local/go/src
 #time ./all.bash 2>&1
 time ./make.bash 2>&1
 cd ../..
-tar cfz go${GO_VERSION}.linux-armv${GOARM}.tar.gz go
+tar cfz "go${GO_VERSION}.linux-armv${GOARM}.tar.gz" go
 popd
-mv /usr/local/go${GO_VERSION}.linux-armv${GOARM}.tar.gz .
+mv "/usr/local/go${GO_VERSION}.linux-armv${GOARM}.tar.gz" .
 
 # cleanup
-rm -fr $GOROOT_BOOTSTRAP
+rm -fr "$GOROOT_BOOTSTRAP"
 rm -fr /usr/local/go
