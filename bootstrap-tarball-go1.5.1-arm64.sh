67965ced2506e79f0c084b6a1720ffe470afb301
Dieter Reuter <dieter.reuter@me.com>
2016-01-02 19:00:55 +0100

    Improve bash syntax according to shellcheck

--- bootstrap-tarball-go1.5.1-arm64.sh
+++ bootstrap-tarball-go1.5.1-arm64.sh
@@ -18,15 +18,15 @@ curl -sSL "https://github.com/hypriot/golang-armbuilds/releases/download/v${GO_B
 
 # clone Go 1.5.x source
 GO_VERSION=${GO_VERSION:-1.5.1}
-rm -fr $HOME/go
-git clone https://go.googlesource.com/go $HOME/go
+rm -fr "$HOME/go"
+git clone https://go.googlesource.com/go "$HOME/go"
 
 # now compile Go 1.5.x and package it as a tarball
-pushd $HOME/go/src
+pushd "$HOME/go/src"
 git checkout release-branch.go1.5
 echo "Bootstrap Go for linux/arm64."
 rm -fr ../../go-linux-arm64-bootstrap
-time GOROOT_BOOTSTRAP=$HOME/go1.5 GOOS=linux GOARCH=arm64 ./bootstrap.bash 2>&1
+time GOROOT_BOOTSTRAP="$HOME/go1.5" GOOS=linux GOARCH=arm64 ./bootstrap.bash 2>&1
 #echo "Bootstrap Go for linux/ppc64"
 #rm -fr ../../go-linux-ppc64-bootstrap
 #time GOROOT_BOOTSTRAP=$HOME/go1.5 GOOS=linux GOARCH=ppc64 ./bootstrap.bash 2>&1
@@ -34,9 +34,9 @@ cd ../..
 pwd
 ls -al go-linux-*-bootstrap.tbz
 popd
-mv $HOME/go-linux-*-bootstrap.tbz .
+mv "$HOME/go-linux-*-bootstrap.tbz" .
 
 # cleanup
-rm -fr $HOME/go
-rm -fr $HOME/go1.5
-rm -fr $HOME/go-linux-*-bootstrap
+rm -fr "$HOME/go"
+rm -fr "$HOME/go1.5"
+rm -fr "$HOME/go-linux-*-bootstrap"
